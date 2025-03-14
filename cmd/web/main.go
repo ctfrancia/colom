4df6c14c4c81c72fcb2d78cb42f7702d16739f08
Christian Francia <christian.francia@tui.com>
2025-03-14 12:33:16 +0100

    stuff

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"time"
+)
+
+type application struct {
+	templateCache map[string]*template.Template
+}
 
 func main() {
 	fmt.Println("Hello, World!")
+	templateCache, err := newTemplateCache("./ui/html")
+	if nil != err {
+		log.Fatal(err)
+	}
+	app := application{templateCache}
+	srv := &http.Server{
+		Addr:         ":4000",
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+	err = srv.ListenAndServe()
+	if nil != err {
+		log.Fatal(err)
+	}
 }
---
+++ cmd/web/templates.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"html/template"
+	"path/filepath"
+	"time"
+)
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
+// humanDate takes a UTC time and returns a formatted string for better human legibility
+func humanDate(t time.Time) string {
+	if t.IsZero() {
+		return ""
+	}
+	return t.UTC().Format("02 Jan 2006 at 15:04")
+}
+
+func newTemplateCache(dir string) (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
+}
