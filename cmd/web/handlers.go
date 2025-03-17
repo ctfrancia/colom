6f66278958366d068bf258b6f5e7937155fe671c
Christian Francia <christian.francia@tui.com>
2025-03-17 17:01:26 +0100

    send form-data to acta

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -29,3 +29,15 @@ func (app *application) login(w http.ResponseWriter, r *http.Request) {
 
 	// app.render(w, r, "login.page.tmpl", &templateData{})
 }
+
+func (app *application) submitActa(w http.ResponseWriter, r *http.Request) {
+	data := r.PostForm
+
+	fmt.Println("data", data)
+	if r.URL.Path != "/submit-acta" {
+		// app.notFound(w)
+		return
+	}
+
+	fmt.Println("submit acta")
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,6 +12,7 @@ func (app application) routes() http.Handler {
 	r.Use(middleware.Logger)
 	r.Get("/", app.home)
 	r.Post("/login", app.login)
+	r.Post("/submit-acta", app.submitActa)
 
 	return r
 }
